08736a59bbd064262f056861d8ebd2377c15a776
Neal Kinkead <nealkinkead@gmail.com>
2024-03-26 13:19:51 -0600

    first commit

--- initialize_repo.bat
+++ initialize_repo.bat
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin git@github.com:hneal98/NW-Comment-System.git
+git remote add origin https://github.com/hneal98/NW-Comment-System.git
 git push -u origin main
